ff3482a7b3d90a5345a5d2dfc7a7ccf0a5a39ade
Max Pan <26495595+MaxxPan@users.noreply.github.com>
2026-06-03 23:06:21 +0800

    feat: add post pages

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,7 +1,12 @@
+import type { CollectionEntry } from 'astro:content';
 import { getCollection } from 'astro:content';
 
 export async function getPublishedPosts() {
   const posts = await getCollection('posts', ({ data }) => !data.draft);
 
   return posts.sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
+}
+
+export function getPostPath(post: CollectionEntry<'posts'>) {
+  return `/posts/${encodeURIComponent(post.id)}/`;
 }
