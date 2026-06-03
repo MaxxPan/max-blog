7c14a525081642d8d75e084ac8d138e99daa44d8
Max Pan <26495595+MaxxPan@users.noreply.github.com>
2026-06-03 22:57:49 +0800

    feat: add base layout

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,16 @@
 import astro from 'eslint-plugin-astro';
+import tsParser from '@typescript-eslint/parser';
 
 export default [
   ...astro.configs.recommended,
+  {
+    files: ['**/*.astro'],
+    languageOptions: {
+      parserOptions: {
+        parser: tsParser,
+      },
+    },
+  },
   {
     ignores: ['dist/**', '.astro/**'],
   },
